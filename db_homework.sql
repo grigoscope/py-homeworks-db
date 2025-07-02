9a2f24fa7b2534a09df19a0d2f19b2f242e089bc
Grigoriy Chepel <grigoriychepel1@gmail.com>
2025-07-02 09:57:18 +0400

    Update db_homework.sql

--- db_homework.sql
+++ db_homework.sql
@@ -1,57 +1,57 @@
 -- Genres
 CREATE TABLE IF NOT EXISTS genres (
     genre_id 		SERIAL 			PRIMARY KEY,
-    name			VARCHAR (60)	NOT NULL
+    name		VARCHAR (60)		NOT NULL
 );
 
 -- Artists
 CREATE TABLE IF NOT EXISTS artists (
     artist_id 		SERIAL 			PRIMARY KEY,
-    name			VARCHAR (60)	NOT NULL
+    name		VARCHAR (60)		NOT NULL
 );
 
 -- Genres and Artists link (many-to-many)
 CREATE TABLE IF NOT EXISTS genres_artists (
-    genre_id		INT				NOT NULL REFERENCES genres (genre_id),
-    artist_id		INT				NOT NULL REFERENCES artists (artist_id),
-					PRIMARY KEY (genre_id, artist_id)
+    genre_id		INT			NOT NULL REFERENCES genres (genre_id),
+    artist_id		INT			NOT NULL REFERENCES artists (artist_id),
+			PRIMARY KEY (genre_id, artist_id)
 );
 
 -- Albums
 CREATE TABLE IF NOT EXISTS albums (
     album_id		SERIAL			PRIMARY KEY,
-    name 			VARCHAR (60)	NOT NULL,
-    year			SMALLINT		NOT NULL
-					CHECK (year >= 1900)
+    name 		VARCHAR (60)		NOT NULL,
+    year		SMALLINT		NOT NULL
+			CHECK (year >= 1900)
 );
 
 -- Artists and Albums link (many-to-many)
 CREATE TABLE IF NOT EXISTS artists_albums (
-    artist_id		INT				NOT NULL REFERENCES artists (artist_id),
-    album_id		INT				NOT NULL REFERENCES albums (album_id),
-					PRIMARY KEY (artist_id, album_id)
+    artist_id		INT			NOT NULL REFERENCES artists (artist_id),
+    album_id		INT			NOT NULL REFERENCES albums (album_id),
+			PRIMARY KEY (artist_id, album_id)
 );
 
 -- Tracks
 CREATE TABLE IF NOT EXISTS tracks (
     track_id 		SERIAL 			PRIMARY KEY,
-    name			VARCHAR(80)		NOT NULL,
+    name		VARCHAR(80)		NOT NULL,
     duration		INTEGER 		NOT NULL,
     album_id 		INT 			NOT NULL REFERENCES albums (album_id),
-					CHECK (duration > 0)
+			CHECK (duration > 0)
 );
 
 -- Playlists
 CREATE TABLE IF NOT EXISTS compilations (
-    compilation_id  SERIAL			PRIMARY KEY,
-    name			VARCHAR(120)	NOT NULL,
-    year			SMALLINT		NOT NULL,
-					CHECK (year >= 1900)
+    compilation_id  	SERIAL			PRIMARY KEY,
+    name		VARCHAR(120)		NOT NULL,
+    year		SMALLINT		NOT NULL,
+			CHECK (year >= 1900)
 );
 
 -- Tracks and albums link (many-to-many)
 CREATE TABLE IF NOT EXISTS compilations_tracks (
-    compilation_id	INT				REFERENCES compilations (compilation_id),
-    track_id		INT				REFERENCES tracks (track_id),
-					PRIMARY KEY (compilation_id, track_id)
+    compilation_id	INT			REFERENCES compilations (compilation_id),
+    track_id		INT			REFERENCES tracks (track_id),
+			PRIMARY KEY (compilation_id, track_id)
 );
